9b2aec0a768dae97e8e3a2f9fdadde1cafe037c9
niehang <1076883711@qq.com>
2021-07-27 21:56:36 +0800

    ajv addKeyword & ajv-18n

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,26 +1,38 @@
 const Ajv = require('ajv')
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
-
-ajv.addFormat('test', (data) => {
-  console.log(data, '----------')
-  return data === 'test'
-})
+const localize = require('ajv-i18n')
 
 const schema = {
   type: 'object',
   properties: {
-    foo: { type: 'string', format: 'email' },
-    bar: { type: 'string', format: 'test' },
+    foo: { type: 'string', test: true },
+    bar: { type: 'string' },
   },
   required: ['foo'],
   additionalProperties: false,
 }
 
+// ajv.addFormat('test', (data) => {
+//   console.log(data, '----------')
+//   return data === 'test'
+// })
+
+ajv.addKeyword('test', {
+  macro() {
+    return {
+      minLength: 10
+    }
+  }
+})
+
 const validate = ajv.compile(schema)
 
 const data = {
-  foo: '123@qq.com',
+  foo: '123@q',
   bar: 'test',
 }
 const valid = validate(data)
-if (!valid) console.log(validate.errors)
+if (!valid) {
+  localize.zh(validate.errors)
+  console.log(validate.errors)
+}
