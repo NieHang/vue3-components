91e9bd5564c92915b0a80db785246d5a27def3f2
benNie <bennie@openrice.com>
2021-07-28 11:53:30 +0800

    completing demo with MonacoEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, Ref, ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
 
-const img = require('./assets/logo.png') // eslint-disable-line
+function toJson(schema: any): string {
+  return JSON.stringify(schema, null, 2)
+}
+
+const schema = {
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
 
 export default defineComponent({
   name: 'App',
   setup() {
-    const state = reactive({
-      name: 'niehang',
-    })
+    const schemaRef: Ref<any> = ref(schema)
+
+    const classRef = useStyles()
+
+    const handleChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (e) {
+        console.log(e)
+      }
+      schemaRef.value = schema
+    }
+
     return () => {
+      const code = toJson(schemaRef.value)
+      const styles = classRef.value
       return (
-        <div id="App">
-          <img src={img} />
-          {state.name}
-          <input type="text" v-model={state.name} />
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleChange}
+            title="Schema"
+            class={styles.editor}
+          />
         </div>
       )
     }
