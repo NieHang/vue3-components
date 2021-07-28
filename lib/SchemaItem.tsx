072243d2b94a42c0f038e622557bddf1bbf478ee
benNie <bennie@openrice.com>
2021-07-28 16:53:22 +0800

    create SchemaForm & SchemaItem

---
+++ lib/SchemaItem.tsx
@@ -0,0 +1,43 @@
+import { defineComponent, PropType } from 'vue'
+import NumberField from './fields/NumberField'
+import StringField from './fields/StringField'
+import { Schema, SchemaTypes } from './types'
+
+export default defineComponent({
+  name: 'SchemaItem',
+  props: {
+    schema: {
+      type: Object as PropType<Schema>,
+      required: true,
+    },
+    value: {
+      required: true,
+    },
+    onChange: {
+      type: Function as PropType<(v: any) => void>,
+      required: true,
+    },
+  },
+  setup(props) {
+    return () => {
+      const schema = props.schema
+
+      // TODO: 如果 type 没有指定，我们需要猜测 type
+      const type = schema.type
+
+      let Component: any
+
+      switch (type) {
+        case SchemaTypes.STRING:
+          Component = StringField
+          break
+        case SchemaTypes.NUMBER:
+          Component = NumberField
+          break
+        default:
+          console.warn(`${type} is not supported`)
+      }
+      return <Component {...props} />
+    }
+  },
+})
